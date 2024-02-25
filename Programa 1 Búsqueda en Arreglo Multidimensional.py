d39f622828b35bce3833ea7ccac2970bd694da1f
Joseph-Math <jl.maldonador@uea.edu.ec>
2024-02-25 15:48:35 -0500

    # Arreglo Bidimensional de 3x3

---
+++ Programa 1 Búsqueda en Arreglo Multidimensional.py
@@ -0,0 +1,15 @@
+# Arreglo Bidimensional de 3x3
+student_grades = [
+    [20, 21, 22],
+    [23, 24, 25],
+    [26, 27, 28]
+]
+
+# Accessing and printing the grades
+print("Student 1 Grades:", student_grades[0])
+print("Student 2 Grades:", student_grades[1])
+print("Student 3 Grades:", student_grades[2])
+
+# Accessing a specific grade
+grade = student_grades[1][2]  # Student 2's third subject grade
+print("Student 2's Grade in Subject 3:", grade)
