0133809355a12e61b092d5ab607ddc8346cc2eab
Joseph-Math <jl.maldonador@uea.edu.ec>
2024-02-25 17:36:47 -0500

    Actualización tarea

--- Tareas/Programa 1 Búsqueda en Arreglo Multidimensional.py
+++ Tareas/Programa 1 Búsqueda en Arreglo Multidimensional.py
@@ -1,14 +1,20 @@
-# Arreglo Dimensional
-student_grades = [
+#Arreglo Dimensional
+lista = [
     [20, 21, 22],
     [23, 24, 25],
     [26, 27, 28]
 ]
-# Accessing and printing the grades
-print("Student 1 Grades:", student_grades[0])
-print("Student 2 Grades:", student_grades[1])
-print("Student 3 Grades:", student_grades[2])
+no_fila = 0
+busqueda = int(input('Ingrese un número entre 20 y 28: '))
+for fila in lista:
+    no_columna = 0
+    for columna in fila:
+        if (columna == busqueda):
+            print(f'El número ingresado es {busqueda}, y pertenece a la posición [{no_fila}][{no_columna}]')
+            no_columna += 1
+    no_fila += 1
 
-# Accessing a specific grade
-grade = student_grades[1][2]  # Student 2's third subject grade
-print("Student 2's Grade in Subject 3:", grade)
+
+
+
+
