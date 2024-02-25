2110a421592c67213a795cd0a64fd8e3d0a2d811
Joseph-Math <jl.maldonador@uea.edu.ec>
2024-02-25 17:43:23 -0500

    Actualización tarea 1 Actualización tarea 2

---
+++ Tareas/Programa 2 Ordenación de Arreglo Multidimensional.py
@@ -0,0 +1,32 @@
+# Matriz bidimensional 3x3
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Función para ordenar una fila de manera ascendente utilizando Bubble Sort
+def bubble_sort_fila(fila):
+    n = len(fila)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if fila[j] > fila[j + 1]:
+                fila[j], fila[j + 1] = fila[j + 1], fila[j]  # Intercambiar elementos
+
+# Función para mostrar la matriz
+def mostrar_matriz(matriz):
+    for fila in matriz:
+        print(fila)
+
+# Mostrar la matriz original
+print("Matriz Original:")
+mostrar_matriz(matriz)
+
+# Ordenar cada fila de la matriz utilizando Bubble Sort
+for fila in matriz:
+    bubble_sort_fila(fila)
+
+# Mostrar la matriz ordenada
+print("\nMatriz Ordenada por Filas:")
+mostrar_matriz(matriz)
+#Gracias
